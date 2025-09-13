09c722a41cffa5a1ede3a309e5e8ed7a0f5d6b99
jeon_1 <jmh4297@naver.com>
2025-09-14 07:30:39 +0900

    Initial commit: Todo Backend API with MongoDB Atlas

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongo_url: 'mongodb+srv://jmh17300_db_user:jmh17300@cluster0.jrwsaqj.mongodb.net/'
+  mongo_url: 'mongodb+srv://jmh17300_db_user:jmh17300@cluster0.jrwsaqj.mongodb.net/todo-backend'
 };
